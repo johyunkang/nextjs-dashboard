fc55fc2b0461c9a146fb989997e4fbc9e196544c
johyunkang <javajo82@gmail.com>
2025-04-07 13:35:39 +0900

    chapter 12 done

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,7 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+const INVOICE_URL = '/dashboard/invoices'
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 const FormSchema = z.object({
@@ -26,13 +27,40 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
     console.log('>>> customer_id', customerId, ', amount:', amountInCents, ', status:', status);
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices')
+    revalidatePath(INVOICE_URL);
+    redirect(INVOICE_URL)
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+
+    revalidatePath(INVOICE_URL);
+    redirect(INVOICE_URL);
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`
+        DELETE FROM invoices WHERE id = ${id}
+    `;
+    revalidatePath(INVOICE_URL);
 }
