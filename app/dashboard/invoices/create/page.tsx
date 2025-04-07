f132ebdfe4e6622414f2ed99c3459388adcc4255
johyunkang <javajo82@gmail.com>
2025-04-07 12:56:07 +0900

    chapter 12의 insert 까지

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,6 @@
-import { fetchCustomers } from '@/app/lib/data'
+import { fetchCustomers } from '@/app/lib/data';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import Form from '@/app/ui/invoices/create-form'
+import Form from '@/app/ui/invoices/create-form';
 
 
 export default async function Page() {
